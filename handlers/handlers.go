130251795167c9b28cf893fc305e3ed08912518f
Jim Rush <jimsrush@Jamess-MacBook-Pro.local>
2017-11-26 13:31:31 +1300

    Updated handler.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,5 +1,7 @@
 package handlers
 
-func HandleMessage(*RTM r) {
-
+//HandleMessage is the handler for a Real Time Slack message Struct
+func HandleMessage(s string) string {
+  // fmt.Print("RMr)
+  return "Hey"
 }
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	handlers "./handlers"
+	// "github.com/jimsrush/slackbot/handlers"
 	"github.com/nlopes/slack"
 )
 
@@ -14,8 +18,25 @@ func main() {
 	go rtm.ManageConnection()
 
 	for {
-		msg := <-rtm.IncomingEvents
-		// fmt.Println("Message is: ", msg)
-		handlers.HandleMessage(msg)
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %v\n", ev)
+				info := rtm.GetInfo()
+				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+
+				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+					response := handlers.HandleMessage(ev.Text)
+					rtm.SendMessage(rtm.NewOutgoingMessage(response, ev.Channel))
+
+				}
+
+			default:
+				//Take no action
+			}
+		}
 	}
 }
