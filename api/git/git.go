da11165f6e84887ded3de332c20491c57744b20a
Jim Rush <jimsrush@Jamess-MacBook-Pro.local>
2017-11-27 16:42:48 +1300

    Added text template

--- api/git/git.go
+++ api/git/git.go
@@ -1,15 +1,25 @@
 package git
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
+	"text/template"
+	"time"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
 var client github.Client
+var repos []*github.Repository
+
+const pullTemplate = `
+Title: ".Title"
+Date: ".Created"
+URL: ".URL"
+`
 
 //InitGit connects to github
 func InitGit() {
@@ -23,19 +33,36 @@ func InitGit() {
 	// get go-github client
 	client = *github.NewClient(tc)
 
-	lo := github.ListOptions{
-		Page:    1,
-		PerPage: 1,
+	// o := &github.RepositoryListByOrgOptions{}
+
+	repos, _, _ = client.GetRepositories().ListByOrg(ctx, "homesnz", &github.RepositoryListByOrgOptions{})
+	for _, repo := range repos {
+		fmt.Println("Repo is:", repo.GetName())
 	}
+
 	opt := &github.PullRequestListOptions{
 		State:       "open",
-		ListOptions: lo,
+		ListOptions: github.ListOptions{},
 	}
 
-	repo, response, err := client.PullRequests.List(ctx, "homesnz", "homes-web", opt)
+	requests, _, err := client.PullRequests.List(ctx, "homesnz", "homes-web", opt)
 
-	fmt.Println("REsponse is", response)
-	fmt.Println("REsponse is", repo)
+	for _, req := range requests {
+		s := SimplePR{
+			Title:   req.GetTitle(),
+			Created: req.GetCreatedAt(),
+			URL:     req.GetURL(),
+		}
+		// fmt.Println("Simple object is simple", s)
+		formatted := FormatPR(s)
+		fmt.Println("Simple PR:", formatted)
+		fmt.Println("Simple PR:", s)
+
+		// fmt.Println("request is", req.GetTitle())
+		// fmt.Println("request is", req.GetCreatedAt())
+		// fmt.Println("request is", req.GetURL())
+		// fmt.Println("request is", req.GetURL())
+	}
 
 	if err != nil {
 		fmt.Printf("Problem in getting repository information %v\n", err)
@@ -43,6 +70,17 @@ func InitGit() {
 	}
 }
 
-func GetPullRequests() {
+type SimplePR struct {
+	Title   string
+	Created time.Time
+	URL     string
+}
 
+func FormatPR(s SimplePR) string {
+	var doc bytes.Buffer
+	t := template.New("PullRequest")
+	t, _ = t.Parse(pullTemplate)
+	t.Execute(&doc, s)
+	st := doc.String()
+	return st
 }
